5ea4fed76689f94cb311d0e97de849f3a5be2d91
Celery <celian.giaume@gmail.com>
2025-11-22 16:52:46 +0100

    added infinite fall

--- UnijamProject/Assets/Script/FallingObstacle.cs
+++ UnijamProject/Assets/Script/FallingObstacle.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class FallingObstacle : MonoBehaviour
 {
+    [SerializeField] private bool fallInfinite;
+
     private bool isFalling = false;
+    private Vector3 originPos;
+    private Quaternion originRotation;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        this.originPos=GetComponent<Transform>().position;
+        this.originRotation=GetComponent<Transform>().rotation;
         
     }
 
@@ -18,7 +25,7 @@ public class FallingObstacle : MonoBehaviour
         
     }
 
-    public void setFalling(bool newState)
+    private void setFalling(bool newState)
     {
         this.isFalling=newState;
     }
@@ -28,6 +35,23 @@ public class FallingObstacle : MonoBehaviour
         GetComponent<Rigidbody2D>().gravityScale=1;
         this.setFalling(true);
     }
+
+    private void stopFalling()
+    {
+        //GetComponent<Rigidbody2D>().gravityScale=0;
+        this.setFalling(false);
+    }
+
+    private void TPOrigin()
+    {
+        Debug.Log("TP");
+        GetComponent<Transform>().position=this.originPos;
+        GetComponent<Transform>().rotation=this.originRotation;
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        GetComponent<Rigidbody2D>().angularVelocity = 0f;
+
+    }
+
 
 
     void OnCollisionEnter2D(Collision2D other)
@@ -40,8 +64,15 @@ public class FallingObstacle : MonoBehaviour
             }
         }
         else 
-        {
-            this.setFalling(false);
+        {   
+            if (!fallInfinite){
+                this.stopFalling();
+            }
+            else
+            {
+                this.TPOrigin();
+            }
+            
         }
     }
 
