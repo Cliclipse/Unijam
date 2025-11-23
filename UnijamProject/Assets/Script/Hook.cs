717439a6bf288620a4c39f09e912581d49e445bf
Cliclipse <120763901+Cliclipse@users.noreply.github.com>
2025-11-23 15:40:15 +0100

    ongle

--- UnijamProject/Assets/Script/Hook.cs
+++ UnijamProject/Assets/Script/Hook.cs
@@ -8,6 +8,7 @@ public class Hook : MonoBehaviour
     [SerializeField] private float speed = 90f;
     [SerializeField] private float maxRange = 20f;
     [SerializeField] private float minRange = 0.25f;
+    
 
 
     public Grappin player;
@@ -18,6 +19,7 @@ public class Hook : MonoBehaviour
     void Start()
     {
         _onTarget = false;
+        transform.LookAt(direction);
     }
 
     void FixHook(Vector2 contactPoint)
