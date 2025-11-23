ffeb9da59262ed4e05139589801e316adac09670
Artemislegende <baptiste.scherz@orange.fr>
2025-11-23 09:34:25 +0100

    son4

--- UnijamProject/Assets/Script/Cafard.cs
+++ UnijamProject/Assets/Script/Cafard.cs
@@ -9,18 +9,25 @@ public class Cafard : MonoBehaviour
     public float speed = 5f;
     private Transform target; // Déplacement vers lequel on essaie d'aller à chaque tour. 
     private Vector3 cible;
+    [SerializeField] private AudioSource blatteSound;
     void Start()
     {
         target = GameObject.FindGameObjectWithTag ("Player").transform;
     }
     void Update()
     {
-       cible = target.position;
-       cible.y = transform.position.y;
-       transform.position = Vector3.MoveTowards(this.transform.position, cible, speed * Time.deltaTime);
-
+        cible = target.position;
+        cible.y = transform.position.y;
+        transform.position = Vector3.MoveTowards(this.transform.position, cible, speed * Time.deltaTime);
+        son_distance();
     }
 
+    void son_distance()
+    {
+        float distance= Vector3.Distance(Move.Instance.transform.position, this.transform.position);
+        Debug.Log(distance);
+        blatteSound.volume = 1 / (distance/5);       
+    }
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
