d95273d98c324706646d8fc842d053d63b823b37
ExoRoy <nayachid@hotmail.fr>
2025-11-23 12:52:39 +0100

    niveau 2 L  modif

--- UnijamProject/Assets/Script/Cafard.cs
+++ UnijamProject/Assets/Script/Cafard.cs
@@ -9,23 +9,36 @@ public class Cafard : MonoBehaviour
     public float speed = 5f;
     private Transform target; // Déplacement vers lequel on essaie d'aller à chaque tour. 
     private Vector3 cible;
+    private Vector3 posOrigin;
+    private Vector3 posTarget;
+    [SerializeField] private float largeur = 10f;
+    [SerializeField] private bool ciblant = false;
     [SerializeField] private AudioSource blatteSound;
     void Start()
     {
+        posOrigin = transform.position;
+        posTarget = transform.position;
+        posTarget[0] += largeur;
+        if (!ciblant)
+        {
+            StartCoroutine(move());
+        }
         target = GameObject.FindGameObjectWithTag ("Player").transform;
     }
     void Update()
     {
         cible = target.position;
         cible.y = transform.position.y;
-        transform.position = Vector3.MoveTowards(this.transform.position, cible, speed * Time.deltaTime);
+        if (ciblant)
+        {
+            transform.position = Vector3.MoveTowards(this.transform.position, cible, speed * Time.deltaTime);
+        }
         son_distance();
     }
 
     void son_distance()
     {
         float distance= Vector3.Distance(Move.Instance.transform.position, this.transform.position);
-        Debug.Log(distance);
         blatteSound.volume = 1 / (distance/5);       
     }
     void OnCollisionEnter2D(Collision2D collision)
@@ -37,6 +50,40 @@ public class Cafard : MonoBehaviour
         else if (collision.gameObject.CompareTag("Flash"))
         {
             Debug.Log("cafard mort");
+        }
+    }
+    
+    IEnumerator move()
+    {
+        while (enVie)
+        {
+            while (transform.position != posTarget)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, posTarget, speed * Time.deltaTime);
+                yield return new WaitForEndOfFrame();
+            }
+
+            Debug.Log("Mehh");
+
+            while (transform.position != posOrigin)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, posOrigin, speed * Time.deltaTime);
+                yield return new WaitForEndOfFrame();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            DeathManager.Instance.ResetScene(false);
+        }
+        else if (other.gameObject.CompareTag("Flash"))
+        {
+            Debug.Log("cafard mort");
+            GetComponent<Collider2D>().enabled = false;
+            speed = 0;
         }
     }
 }
